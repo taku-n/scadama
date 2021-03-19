7ce880e41c3e679181b7e0ce3f2b4d018dbffd1a
Takuto Nanjo <16991946+taku-n@users.noreply.github.com>
2021-03-19 10:54:04 +0900

    strange bug

--- impl.py
+++ impl.py
@@ -1,6 +1,6 @@
 from multiprocessing import *
-from queue import Queue
 from threading import *
+from time import sleep
 
 import MetaTrader5 as mt5
 import toml
@@ -58,6 +58,20 @@ def connect(instance):
         else:
             instance.combobox_symbol.SetSelection(0)
             instance.symbol = instance.combobox_symbol.GetStringSelection()
+
+        instance.q_ctrl = Queue()  # Interprocess Communication for controlling a process.
+        q_tick = Queue()  # Interprocess Communication for ticks.
+
+        # A thread to receive a tick.
+        thread = Thread(target = recv_tick, args = (instance, q_tick))
+        thread.start()
+
+        # A process to send a tick.
+        process = Process(target = send_tick, args = (instance.q_ctrl, q_tick))
+        process.start()
+        instance.q_ctrl.put(instance.choice_client.GetStringSelection())
+        instance.q_ctrl.put(instance.symbol)
+
     else:  # Failure
         instance.togglebutton_connect.SetValue(False)
         print('Failed to connect to', client)
@@ -65,9 +79,117 @@ def connect(instance):
 def disconnect(instance):
     client = instance.choice_client.GetStringSelection()
 
+    instance.q_ctrl.put('disconnect')  # Child Process Disconnection
     mt5.shutdown()
     print('Disconnected from', client)
     instance.SetStatusText('Disconnected from ' + client)
     instance.choice_client.Enable()
     instance.combobox_symbol.Clear()
     instance.togglebutton_connect.SetLabel('Connect')
+
+def recv_tick(instance, q_tick):  # Runs in a thread.
+    while True:
+        # Get a tick.
+
+        tick = q_tick.get()
+        print(tick)
+        continue
+
+        # Show the servers local time.
+
+        try:
+            time = tick['time']
+        except Exception as e:
+            if e.args != ():
+                pass
+                #write_log(f'Error, getting a time: {e.args}')
+            sys.exit()
+
+        # This doesnt mean it is UTC but a servers local time.
+        time_zone = datetime.timezone(datetime.timedelta(hours = 0))
+
+        time = datetime.datetime.fromtimestamp(time, time_zone)
+        time = f'{time:%Y.%m.%d %a %H:%M:%S}'  # f-string
+
+        try:
+            label_time.configure(text = time)
+        except Exception as e:
+            if e.args != ():
+                write_log(e.args)
+            sys.exit()
+
+        # Set a symbol.
+        Tick.symbol = tick['symbol']
+
+        # Show ask, bid and spread.
+
+        try:
+            ask = tick['ask']
+        except Exception as e:
+            if e.args != ():
+                write_log(e.args)
+            sys.exit()
+
+        Tick.ask = ask
+
+        try:
+            bid = tick['bid']
+        except Exception as e:
+            if e.args != ():
+                write_log(e.args)
+            sys.exit()
+
+        Tick.bid = bid
+
+        spread = (ask - bid) * 10000.0
+        Tick.spread = spread
+
+        try:
+            button_ask.configure(text = f'{ask:.5f}')
+        except Exception as e:
+            if e.args != ():
+                write_log(e.args)
+            sys.exit()
+
+        try:
+            button_bid.configure(text = f'{bid:.5f}')
+        except Exception as e:
+            if e.args != ():
+                write_log(e.args)
+            sys.exit()
+
+        try:
+            label_spread.configure(text = f'{spread:.1f}')
+        except Exception as e:
+            if e.args != ():
+                write_log(e.args)
+            sys.exit()
+
+def send_tick(q_ctrl, q_tick):  # Runs in a child process.
+    client = q_ctrl.get()
+
+    if mt5.initialize(client):  # Success
+        symbol = q_ctrl.get()
+        last_tick = None
+
+        while True:
+            if not q_ctrl.empty():
+                ctrl = q_ctrl.get()
+                if ctrl == 'disconnect':
+                    mt5.shutdown()
+                    return
+                else:
+                    symbol = ctrl
+
+            tick = mt5.symbol_info_tick(symbol)._asdict()  # Non-blocking
+
+            if tick != last_tick:
+                q_tick.put('----')
+                q_tick.put(tick)
+                q_tick.put(last_tick)
+                q_tick.put(tick == last_tick)
+                last_tick = tick
+                tick['symbol'] = symbol
+                q_tick.put(tick)
+
+            sleep(0.001)
